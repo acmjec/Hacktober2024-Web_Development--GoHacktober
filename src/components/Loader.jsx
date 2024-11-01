0ef0d3d129a7c18b2eab73049477f16c22bdd548
ghat0tkach <awesomevikram3@gmail.com>
2024-11-01 19:29:33 +0530

    feat:added loader component
    Signed-off-by: ghat0tkach <awesomevikram3@gmail.com>

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+// SpinnerLoader.jsx
+import React from 'react';
+
+const SpinnerLoader = () => {
+  return (
+    <div className="w-10 h-10 border-4 border-black border-t-white rounded-full animate-spin"></div>
+  );
+};
+
+export default SpinnerLoader;
